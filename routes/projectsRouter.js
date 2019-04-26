48bb6b3be316b18cd7c57b824e9b9b473d1ad2b3
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-04-26 10:55:22 -0700

    all crud operations functional, mvp

--- routes/projectsRouter.js
+++ routes/projectsRouter.js
@@ -1,1 +1,98 @@
 const express = require('express');
+const router = express.Router();
+const projectsDb = require('../data/helpers/projectModel');
+const actionsDb = require('../data/helpers/actionModel');
+
+router.get("/", (req, res) => {
+    projectsDb.get()
+    .then(response => {
+        res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err, message: "Projects cannot be retrieved" });
+    })
+});
+
+router.get("/:id", (req, res) => {
+    const projectId = req.params.id;
+    projectsDb.get(projectId)
+    .then(response => {
+        if (!response) {
+          res.status(404).json({ message: "The project with specified ID was not found" });
+        } else {
+          res.status(200).json(response);
+        }
+    })
+    .catch(err => {
+      res.status(500).json({ error: err, message: "There was an error trying to find the project." });
+    })
+});
+
+router.post("/",  (req, res) => {
+    const project = req.body;
+    if (project.description && project.name) {
+        projectsDb.insert(project)
+        .then(response => {
+            res.status(200).json({ response, message: "New project was succesfuly created"});
+        })
+        .catch(err => {
+            res.status(500).json({ error: err, message: "There was an error trying to create a new project"});
+        })
+    } else {
+        res.status(400).json({ errorMessage: "Please provide description and name for project." });
+    };
+});
+
+router.delete("/:id", (req, res) => {
+    const projectId = req.params.id;
+    projectsDb.remove(projectId)
+    .then(response => {
+        if (response) {
+            res.status(200).json(response);
+        } else {
+            res.status(404).json({ message: "The project with specified ID was not found" });
+        }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "There was an error trying to delete the project from the database."});
+    })
+});
+
+router.put("/:id", (req, res) => {
+    const projectId = req.params.id;
+    const update = req.body;
+    if (update.name || update.description) {
+        projectsDb.update(projectId, update)
+        .then(response => {
+            if (response) {
+                res.status(200).json({ response, message: `project has been updated` });
+            } else {
+                res.status(404).json({ message: "The project with specified ID was not found" });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: err, message: "The project information cannot be modified."});
+        })
+    } else if (!update.name || update.description) {
+        res.status(400).json({ errorMessage:"Please update description and/or name for the project." })
+    }
+});
+
+router.get('/:projectId/actions', async (req, res) => {
+    const { projectId } = req.params;
+    try {
+        const project = await projectsDb.get(projectId);
+        if(!project) {
+            res.status(404).json({message: "The project with specified ID could not be found"});
+        } else {
+            const actions = await actionsDb.get();
+            res.json(actions);
+        }
+    }
+    catch (err) {
+        res.status(500).json({message: "There was an error trying to retreive the actions for the provided project"})
+    }
+}); 
+
+module.exports = router; 
